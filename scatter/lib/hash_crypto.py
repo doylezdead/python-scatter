4cc24bbba89df2162b73407eedfc8ad9e9d7260c
Ryan Doyle <rcdoyle@mtu.edu>
2016-09-12 23:35:39 -0700

    re-adding lib from laptop

---
+++ scatter/lib/hash_crypto.py
@@ -0,0 +1,15 @@
+import hashlib
+import pycrypto
+import time
+import os
+
+def ip_hash(ip):
+    new_hash = hashlib.sha1()
+    new_hash.update(ip.encode('utf-8'))
+    return new_hash.hexdigest()
+
+def unique_hash():
+    new_hash = hashlib.sha1()
+    new_hash.update(str(os.urandom(64)).encode('utf-8'))
+    return new_hash.hexdigest()
+
